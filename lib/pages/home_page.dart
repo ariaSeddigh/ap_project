7c4f7321bb21f92ae3d4438f8bc3a541ee394067
ariaSeddigh <aria.seddigh@gmail.com>
2023-05-25 13:03:30 +0330

    navigation bar buttons change pages

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,14 +10,85 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePage extends State<HomePage> {
+  int _currentIndex = 0;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text('HomePage'),
-      ),
+      body: SizedBox.expand(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(
+              () {
+                _currentIndex = index;
+              },
+            );
+          },
+          children: [
+            //home page
+            SafeArea(
+              child: Container(
+                child: Column(
+                  children: [
+                    Text(
+                      'home page',
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+            ),
 
+            //search page
+            SafeArea(
+              child: Container(
+                child: Column(
+                  children: [
+                    SafeArea(
+                        child: Text(
+                      'search page',
+                      style: TextStyle(fontSize: 20),
+                    ))
+                  ],
+                ),
+              ),
+            ),
+
+            //setting page
+            SafeArea(
+              child: Container(
+                child: Column(children: [
+                  Text('1'),
+                  Text('2'),
+                  Text('3'),
+                  ElevatedButton(onPressed: () {}, child: null),
+                  ElevatedButton(onPressed: () {}, child: null),
+                  ElevatedButton(onPressed: () {}, child: null),
+                ]),
+              ),
+            ),
+          ],
+        ),
+      ),
       bottomNavigationBar: CurvedNavigationBar(
+        height: 50,
+        onTap: (index) {
+          _pageController.jumpToPage(index);
+        },
         items: [
           Icon(
             Icons.home,
